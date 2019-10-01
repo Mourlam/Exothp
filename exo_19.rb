5af9acf0f0d5c86f2f6412fc93723e5d117ea44e
ephilaey <jmourlam12@gmail.com>
2019-10-01 18:32:13 +0200

    final exo19

--- exo_19.rb
+++ exo_19.rb
@@ -2,12 +2,7 @@ array=["jean.dupont.01@email.fr"]
 
 for i in 2..50
 array<<"jean.dupont.0#{i}@email.fr";
-if i == i.to_i;
-  if (i % 2) == 0;
-    puts "#{i}";
-    puts array;
-  else ;
-    puts ".|.";
-end;
+if i%2 == 0;
+  puts "jean.dupont.0#{i}@email.fr";
 end;
 end
