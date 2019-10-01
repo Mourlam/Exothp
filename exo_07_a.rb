a2c5bb0a855295b500a262114d1d24a0d4057158
ephilaey <jmourlam12@gmail.com>
2019-10-01 13:40:13 +0200

    exo7a

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets
+puts user_name
+#gets.chomp créer un champ qui permet à l'utilisateur d'écrire puis le programme renvoi la réponse
+#normalement .chomp permet d'enlever le /n que créer gets seul mais chez moi ça le fait pas ...
