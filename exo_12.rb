e46808fc4aac2035e5106e15f817f7037ca48b95
ephilaey <jmourlam12@gmail.com>
2019-10-01 14:34:19 +0200

    exo12

---
+++ exo_12.rb
@@ -0,0 +1,10 @@
+puts "Donne moi un nombre ..."
+nombre = gets.chomp.to_i
+i=0
+nombre.times do |i|
+print " #{i+1}"
+  end
+#for i in 0...nombre
+#  puts "#{i+1}"
+#end
+#fonctionne de la même manière pour les lignes 4 --> 6
